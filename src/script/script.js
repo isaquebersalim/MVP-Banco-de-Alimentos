a723f42f955fe839ac44ece527b895838e87a14c
Isaque Bersalim <isaquebersalim@gmail.com>
2025-11-09 14:59:11 -0300

    Criando conexão BD.

--- src/script/script.js
+++ src/script/script.js
@@ -783,3 +783,7 @@ function gerarRelatorioEstoque() {
 window.addEventListener("DOMContentLoaded", () => {
         carregarDados();
     });
+
+import { neon } from '@netlify/neon';
+const sql = neon(); // automatically uses env NETLIFY_DATABASE_URL
+const [post] = await sql`SELECT * FROM posts WHERE id = ${postId}`;
